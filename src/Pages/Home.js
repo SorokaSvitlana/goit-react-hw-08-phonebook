64665a0b63a0e70e51278fcc7205b90b347571b4
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-06-26 16:31:04 +0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Typography, Box, List, ListItem, ListItemText } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledHomePage = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+`;
+
+const StyledTitle = styled(Typography)`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+  color: #333;
+`;
+
+const StyledDescription = styled(Typography)`
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+  color: #555;
+`;
 
 export default function Home() {
-    return (
-      <div>
-        <h1> Task manager welcome page </h1>
-      </div>
-    );
-  }
+  return (
+    <StyledHomePage>
+      <StyledTitle>Welcome to Phonebook</StyledTitle>
+      <StyledDescription>Manage your contacts easily with Phonebook</StyledDescription>
+      <List>
+        <ListItem>
+          <ListItemText primary="Create and store your contacts" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Organize contacts with groups and tags" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Search and filter contacts efficiently" />
+        </ListItem>
+      </List>
+    </StyledHomePage>
+  );
+};
+
   
