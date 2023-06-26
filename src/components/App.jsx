87436a14ef103f9e9192349fc9b5a1e3bb0230d5
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-06-26 16:08:59 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect }  from "react";
 import { Container } from './Container/Container.Styled'
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { PrivateRoute } from "./PrivateRoute";
 import { RestrictedRoute } from "./RestrictedRoute";
-import { AppBar, AppBarHeader } from "./AppBar/AppBar";
+import { AppBar} from "./AppBar/AppBar";
 import { refreshUser } from "redux/auth/operation";
 import LoginPage from "Pages/Login";
 import RegisterPage from "Pages/Register";
 import ContactsPage from "Pages/Phonebook";
-import { selectIsRefreshing } from "redux/auth/selectors";
 import Home from "Pages/Home";
 
 export function App() {
